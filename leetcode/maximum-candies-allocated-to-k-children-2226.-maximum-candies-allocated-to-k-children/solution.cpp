d7fea158eef5abe855c6aa99d8467cda33d9fe4b
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-02-25 21:06:58 +0530

    LeetCode: 2226. Maximum Candies Allocated to K Children (maximum-candies-allocated-to-k-children)

---
+++ leetcode/maximum-candies-allocated-to-k-children-2226.-maximum-candies-allocated-to-k-children/solution.cpp
@@ -0,0 +1,36 @@
+// Problem: 2226. Maximum Candies Allocated to K Children
+// URL: https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+// Language: cpp
+class Solution {
+public:
+    typedef long long ll;
+
+    bool HoSktaHai(ll midd , vector<int>& candies , ll k){
+        ll count = 0;
+        ll first = candies[0];
+        for(int i=0; i<candies.size(); i++){
+            count += candies[i] / midd;
+
+            if(count >= k) return true;
+        }
+        return false;
+    }
+
+ 
+    int maximumCandies(vector<int>& candies, long long k) {
+        ll low = 1;
+        ll high = accumulate(candies.begin() , candies.end() , 0LL);
+        ll ans = 0;
+
+        while(low <= high){
+            ll mid = low + (high - low)/2;
+
+            if(HoSktaHai(mid , candies , k)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+};
