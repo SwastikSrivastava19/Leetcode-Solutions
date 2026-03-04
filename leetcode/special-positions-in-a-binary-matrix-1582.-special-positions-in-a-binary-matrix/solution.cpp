b464f75e55443fdde12aa6eb7dae38c82207eaa4
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-04 14:45:15 +0530

    LeetCode: 1582. Special Positions in a Binary Matrix (special-positions-in-a-binary-matrix)

--- leetcode/special-positions-in-a-binary-matrix-1582.-special-positions-in-a-binary-matrix/solution.cpp
+++ leetcode/special-positions-in-a-binary-matrix-1582.-special-positions-in-a-binary-matrix/solution.cpp
@@ -21,7 +21,7 @@ public:
             }
         }
 
-        int ans=0;
+        int ans = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==1 && rowSum[i]==1 && colSum[j]==1) ans++;
