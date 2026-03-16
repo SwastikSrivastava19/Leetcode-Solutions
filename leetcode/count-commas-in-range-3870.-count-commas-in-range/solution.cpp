e589eb6ffb5d2fd22ad042295bdcaf1fcd25179c
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-16 12:20:07 +0530

    LeetCode: 3870. Count Commas in Range (count-commas-in-range)

---
+++ leetcode/count-commas-in-range-3870.-count-commas-in-range/solution.cpp
@@ -0,0 +1,12 @@
+// Problem: 3870. Count Commas in Range
+// URL: https://leetcode.com/problems/count-commas-in-range/
+// Language: cpp
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n < 1000) return 0;
+        // if(n < 100)
+        // if()
+        else return (n - 1000 + 1);
+    }
+};
