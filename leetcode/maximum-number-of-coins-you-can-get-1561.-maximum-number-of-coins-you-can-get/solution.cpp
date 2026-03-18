f50245b4ffe1ff2b19cd9e8f465269e9132d1046
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-18 13:32:42 +0530

    LeetCode: 1561. Maximum Number of Coins You Can Get (maximum-number-of-coins-you-can-get)

--- leetcode/maximum-number-of-coins-you-can-get-1561.-maximum-number-of-coins-you-can-get/solution.cpp
+++ leetcode/maximum-number-of-coins-you-can-get-1561.-maximum-number-of-coins-you-can-get/solution.cpp
@@ -6,9 +6,11 @@ public:
     int maxCoins(vector<int>& piles) {
         sort(piles.begin() , piles.end());
         int ans = 0;
+        int n = piles.size();
 
-        for(int i=piles.size()/3; i<piles.size(); i+=2){
-            ans += piles[i];
+        for(int i=0; i<piles.size()/3; i++){
+            ans += piles[n-2];
+            n -= 2;
         }
         return ans;
     }
