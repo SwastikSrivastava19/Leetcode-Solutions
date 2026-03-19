142d3aaf80fdf8eecdfa1b9f58284a79e77d2be5
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-19 11:47:31 +0530

    LeetCode: 303. Range Sum Query - Immutable (range-sum-query-immutable)

---
+++ leetcode/range-sum-query-immutable-303.-range-sum-query-immutable/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: 303. Range Sum Query - Immutable
+// URL: https://leetcode.com/problems/range-sum-query-immutable/
+// Language: cpp
+class NumArray {
+public:
+vector<int> pre;
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        pre.assign(n+1 , 0);
+        for(int i=1; i<=n; i++){
+            pre[i] = pre[i-1] + nums[i-1];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return pre[right + 1] - pre[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
