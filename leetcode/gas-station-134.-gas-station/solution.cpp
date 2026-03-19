54b646d40aa32859ad72df7c292cd5f7cafc2247
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-19 13:59:34 +0530

    LeetCode: 134. Gas Station (gas-station)

---
+++ leetcode/gas-station-134.-gas-station/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: 134. Gas Station
+// URL: https://leetcode.com/problems/gas-station/
+// Language: cpp
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0 , curr = 0 , start = 0;
+
+        for(int i=0; i<gas.size(); i++){
+            int diff = gas[i] - cost[i];
+
+            curr += diff;
+            total += diff;
+
+            if(curr < 0){
+                start = i + 1;
+                curr = 0;
+            }
+            // if(total < 0) return -1;
+        }
+        if(total < 0) return -1;
+        return start;
+    }
+};
