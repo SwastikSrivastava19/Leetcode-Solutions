03be87d956560fe5981287a71dedacc6f534b767
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-19 15:53:34 +0530

    LeetCode: 2134. Minimum Swaps to Group All 1's Together II (minimum-swaps-to-group-all-1s-together-ii)

---
+++ leetcode/minimum-swaps-to-group-all-1s-together-ii-2134.-minimum-swaps-to-group-all-1s-together-ii/solution.cpp
@@ -0,0 +1,17 @@
+// Problem: 2134. Minimum Swaps to Group All 1's Together II
+// URL: https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/
+// Language: cpp
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int k = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
+        int mx = cnt;
+        for (int i = k; i < n + k; ++i) {
+            cnt += nums[i % n] - nums[(i - k + n) % n];
+            mx = max(mx, cnt);
+        }
+        return k - mx;
+    }
+};
