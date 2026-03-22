e7dc9bb670f3e8047ec2a2feceb47be36f92e1c4
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-22 22:27:04 +0530

    LeetCode: 3875. Construct Uniform Parity Array I (construct-uniform-parity-array-i)

---
+++ leetcode/construct-uniform-parity-array-i-3875.-construct-uniform-parity-array-i/solution.cpp
@@ -0,0 +1,9 @@
+// Problem: 3875. Construct Uniform Parity Array I
+// URL: https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Language: cpp
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
