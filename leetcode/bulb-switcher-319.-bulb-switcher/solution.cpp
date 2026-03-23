bbf3f77f9d88ddb74727d5165489c95276b61088
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-23 15:50:36 +0530

    LeetCode: 319. Bulb Switcher (bulb-switcher)

---
+++ leetcode/bulb-switcher-319.-bulb-switcher/solution.cpp
@@ -0,0 +1,9 @@
+// Problem: 319. Bulb Switcher
+// URL: https://leetcode.com/problems/bulb-switcher/
+// Language: cpp
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return (int)sqrt(n);
+    }
+};
