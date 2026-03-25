ab8072e398003a0b1f844bd60a6e454b5e832dd0
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-25 11:02:31 +0530

    LeetCode: 560. Subarray Sum Equals K (subarray-sum-equals-k)

---
+++ leetcode/subarray-sum-equals-k-560.-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,21 @@
+// Problem: 560. Subarray Sum Equals K
+// URL: https://leetcode.com/problems/subarray-sum-equals-k/
+// Language: cpp
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int , int> mp;
+        int ans = 0 , sum = 0;
+        mp[0] = 1;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+
+            if(mp.find(sum - k) != mp.end()){
+                ans += mp[sum - k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
