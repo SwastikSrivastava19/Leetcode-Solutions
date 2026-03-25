1c07ed1e2a43e7cc703125159979490f85da5464
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-03-25 13:48:15 +0530

    LeetCode: 523. Continuous Subarray Sum (continuous-subarray-sum)

---
+++ leetcode/continuous-subarray-sum-523.-continuous-subarray-sum/solution.cpp
@@ -0,0 +1,28 @@
+// Problem: 523. Continuous Subarray Sum
+// URL: https://leetcode.com/problems/continuous-subarray-sum/
+// Language: cpp
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int , int> mp;
+        mp[0] = -1;
+        int sum = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            int rem = sum % k;
+            // int rem = ((sum % k) + k) % k;
+
+            if(mp.find(rem) != mp.end()){
+                if((i - mp[rem]) >= 2){
+                    return true;
+                }
+                // else mp[rem] = i;
+            }
+            else mp[rem] = i;
+
+            // mp[rem]++;
+        }
+        return false;
+    }
+};
