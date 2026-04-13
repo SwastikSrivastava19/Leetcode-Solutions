5c0e0a7dd62237014d9cff75f7f1ff6e131be42a
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-04-13 14:04:47 +0530

    LeetCode: 77. Combinations (combinations)

--- leetcode/combinations-77.-combinations/solution.cpp
+++ leetcode/combinations-77.-combinations/solution.cpp
@@ -1,28 +1,54 @@
 // Problem: 77. Combinations
 // URL: https://leetcode.com/problems/combinations/
 // Language: cpp
+// class Solution {
+// public:
+//     void f(int n , int k , vector<vector<int>> &ans , vector<int> &ds , int curr){
+//         // base case
+//         if(ds.size() == k){
+//             ans.push_back(ds);
+//             return;
+//         }
+//         if(curr > n) return;
+//         // take
+//         ds.push_back(curr);
+//         f(n , k , ans , ds , curr + 1);
+//         ds.pop_back();
+
+//         // not take
+//         f(n , k , ans , ds , curr + 1);
+//     }
+
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         int curr;
+//         f(n , k , ans , ds , 1);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    void f(int n , int k , vector<vector<int>> &ans , vector<int> &ds , int curr){
+    void f(int n , int k , vector<vector<int>> &ans , vector<int> &ds , int start){
         // base case
         if(ds.size() == k){
             ans.push_back(ds);
             return;
         }
-        if(curr > n) return;
-        // take
-        ds.push_back(curr);
-        f(n , k , ans , ds , curr + 1);
-        ds.pop_back();
-
-        // not take
-        f(n , k , ans , ds , curr + 1);
+        // if()
+        for(int i=start; i<=n; i++){
+            ds.push_back(i);
+            f(n , k , ans , ds , i + 1);
+            ds.pop_back();
+        }
     }
 
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> ds;
-        int curr;
+        int start;
         f(n , k , ans , ds , 1);
         return ans;
     }
