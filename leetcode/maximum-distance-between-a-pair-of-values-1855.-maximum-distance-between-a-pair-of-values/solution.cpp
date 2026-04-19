5300d937224d327bbc0daa40a06a14eac45d878e
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-04-19 10:28:36 +0530

    LeetCode: 1855. Maximum Distance Between a Pair of Values (maximum-distance-between-a-pair-of-values)

---
+++ leetcode/maximum-distance-between-a-pair-of-values-1855.-maximum-distance-between-a-pair-of-values/solution.cpp
@@ -0,0 +1,21 @@
+// Problem: 1855. Maximum Distance Between a Pair of Values
+// URL: https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/
+// Language: cpp
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0, maxDist = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] <= nums2[j]) {
+                maxDist = max(maxDist, j - i);
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return maxDist;
+    }
+};
+
