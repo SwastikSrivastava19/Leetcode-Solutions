4d3b70de5ac916ad6add07194777ef80f756df6b
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-04-20 10:19:11 +0530

    LeetCode: 2078. Two Furthest Houses With Different Colors (two-furthest-houses-with-different-colors)

--- leetcode/two-furthest-houses-with-different-colors-2078.-two-furthest-houses-with-different-colors/solution.cpp
+++ leetcode/two-furthest-houses-with-different-colors-2078.-two-furthest-houses-with-different-colors/solution.cpp
@@ -8,13 +8,25 @@ public:
         if(colors[0] != colors[n-1]) return n-1;
         int ans = INT_MIN;
 
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(colors[i] != colors[j]){
-                    ans = max(ans , abs(i-j));
-                }
-            }
-        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         if(colors[i] != colors[j]){
+        //             ans = max(ans , abs(i-j));
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // better->
+        int l = 0 , r = n - 1;
+        while(colors[l] == colors[r]) l++;
+        ans = max(ans , abs(l-r));
+
+        l=0;
+
+        while(colors[l] == colors[r]) r--;
+        ans = max(ans , abs(l-r));
+
         return ans;
 
     }
