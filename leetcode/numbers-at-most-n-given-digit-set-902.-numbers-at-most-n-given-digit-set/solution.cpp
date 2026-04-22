975514d0b1c233f08317eee0e30c93f8e6090a32
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-04-22 10:36:23 +0530

    LeetCode: 902. Numbers At Most N Given Digit Set (numbers-at-most-n-given-digit-set)

---
+++ leetcode/numbers-at-most-n-given-digit-set-902.-numbers-at-most-n-given-digit-set/solution.cpp
@@ -0,0 +1,35 @@
+// Problem: 902. Numbers At Most N Given Digit Set
+// URL: https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
+// Language: cpp
+class Solution {
+public:
+
+    int f(string &s , int ind , bool tight , bool lz , int mask){
+        if(ind == s.size()) return 1;
+
+        int lb = 0 , ub = (tight ? s[ind] - '0' : 9);
+        int res = 0;
+
+        for(int dig=lb; dig<=ub; dig++){
+            if(lz && dig == 0){
+                res += f(s , ind + 1 , tight && dig == ub , 1 , mask);
+            }
+            else{
+                if(!(mask & (1 << dig))) continue;
+                res += f(s , ind + 1 , tight && dig == ub , 0 , mask);
+            }
+        }
+        return res;
+    }
+
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        int mask = 0;
+        for(auto d : digits){
+            int num = d[0] - '0';   // convert char ->> int
+            mask |= (1 << num);     // mask krdo , set that bit
+        }
+        string s = to_string(n);
+
+        return f(s , 0 , 1 , 1 , mask) - 1;
+    }
+};
