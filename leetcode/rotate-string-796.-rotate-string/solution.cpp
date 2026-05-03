019dbf1ebe1267977f303f8ac0f2105f4a0f99ee
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-03 09:17:27 +0530

    LeetCode: 796. Rotate String (rotate-string)

--- leetcode/rotate-string-796.-rotate-string/solution.cpp
+++ leetcode/rotate-string-796.-rotate-string/solution.cpp
@@ -4,11 +4,19 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i=1; i<=s.size(); i++){
-            string str = s.substr(i) + s.substr(0 , i);
+        // for(int i=1; i<=s.size(); i++){
+        //     string str = s.substr(i) + s.substr(0 , i);
 
-            if(str == goal) return true;
-        }
-        return false;
+        //     if(str == goal) return true;
+        // }
+        // return false;
+        // better approach ->>>
+
+        if (s.size() != goal.size()) return false;
+
+
+        string DoubleSize = s +s;
+
+        return DoubleSize.find(goal) != string::npos;
     }
 };
