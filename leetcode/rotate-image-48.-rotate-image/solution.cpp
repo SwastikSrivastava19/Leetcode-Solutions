524aad87b8daa79e429d2777e504781557a12dc5
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-04 09:31:46 +0530

    LeetCode: 48. Rotate Image (rotate-image)

---
+++ leetcode/rotate-image-48.-rotate-image/solution.cpp
@@ -0,0 +1,17 @@
+// Problem: 48. Rotate Image
+// URL: https://leetcode.com/problems/rotate-image/
+// Language: cpp
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin() , matrix[i].end());
+        }
+    }
+};
