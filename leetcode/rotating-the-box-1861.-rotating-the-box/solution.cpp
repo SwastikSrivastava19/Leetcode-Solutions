0a61a4c70b7efe1e42682b70c32fef82805b96db
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-06 23:28:37 +0530

    LeetCode: 1861. Rotating the Box (rotating-the-box)

---
+++ leetcode/rotating-the-box-1861.-rotating-the-box/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: 1861. Rotating the Box
+// URL: https://leetcode.com/problems/rotating-the-box/
+// Language: cpp
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<char>> res(cols, vector<char>(rows, '.'));
+
+        for (int r = 0; r < rows; r++) {
+            int p = cols - 1;
+            for (int c = cols - 1; c >= 0; c--) {
+                if (grid[r][c] == '*') {
+                    res[c][rows - 1 - r] = '*';
+                    p = c - 1;
+                } else if (grid[r][c] == '#')
+                    res[p--][rows - 1 - r] = '#';                    
+            }
+        }
+
+        return res;
+    }
+};
