e95a2b8e812ca4db3f4a017387fb8c34e6f49600
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-10 22:28:14 +0530

    LeetCode: 2770. Maximum Number of Jumps to Reach the Last Index (maximum-number-of-jumps-to-reach-the-last-index)

---
+++ leetcode/maximum-number-of-jumps-to-reach-the-last-index-2770.-maximum-number-of-jumps-to-reach-the-last-index/solution.cpp
@@ -0,0 +1,45 @@
+// Problem: 2770. Maximum Number of Jumps to Reach the Last Index
+// URL: https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/
+// Language: cpp
+class Solution {
+public:
+    
+    int solve(int i, vector<int>& nums, int target, vector<int>& dp) {
+        int n = nums.size();
+
+        // reached last index
+        if (i == n - 1)
+            return 0;
+
+        // already computed
+        if (dp[i] != -2)
+            return dp[i];
+
+        int ans = -1;
+
+        // try all possible jumps
+        for (int j = i + 1; j < n; j++) {
+
+            if (abs(nums[j] - nums[i]) <= target) {
+
+                int temp = solve(j, nums, target, dp);
+
+                // if reachable
+                if (temp != -1)
+                    ans = max(ans, 1 + temp);
+            }
+        }
+
+        return dp[i] = ans;
+    }
+
+    int maximumJumps(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        // -2 means not calculated yet
+        vector<int> dp(n, -2);
+
+        return solve(0, nums, target, dp);
+    }
+};
