a3b37c80ebd6e2d738be3d49499f5754714b6088
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-22 14:37:24 +0530

    LeetCode: 197. Rising Temperature (rising-temperature)

--- leetcode/rising-temperature-197.-rising-temperature/solution.sql
+++ leetcode/rising-temperature-197.-rising-temperature/solution.sql
@@ -1,25 +1,8 @@
 -- Problem: 197. Rising Temperature
 -- URL: https://leetcode.com/problems/rising-temperature/
 -- Language: mysql
--- Submission ID: dom-1779439701681
--- Submitted At: 2026-05-22T08:48:21.714Z
-# Solution 1
-cnt=0
-while read line && [ $cnt -le 10 ]; do
-  let 'cnt = cnt + 1'
-  if [ $cnt -eq 10 ]; then
-    echo $line
-    exit 0
-  fi
-done < file.txt
-
-# Solution 2
-awk 'FNR == 10 {print }'  file.txt
-# OR
-awk 'NR == 10' file.txt
-
-# Solution 3
-sed -n 10p file.txt
-
-# Solution 4
-tail -n+10 file.txt|head -1
+-- Submission ID: dom-1779440838423
+-- Submitted At: 2026-05-22T09:07:18.486Z
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
