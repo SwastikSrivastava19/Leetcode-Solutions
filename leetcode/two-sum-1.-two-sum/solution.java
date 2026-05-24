9f0f7b7d17a5b23b53aa746e84c928be857c3dfa
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-24 11:55:49 +0530

    LeetCode: 1. Two Sum (two-sum)

---
+++ leetcode/two-sum-1.-two-sum/solution.java
@@ -0,0 +1,21 @@
+// Problem: 1. Two Sum
+// URL: https://leetcode.com/problems/two-sum/
+// Language: java
+// Submission ID: dom-1779603945161
+// Submitted At: 2026-05-24T06:25:45.666Z
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i,j;
+        for(i=0; i<n;i++){
+            int first= nums[i];
+            for (j=i+1; j<n; j++){
+                int second= nums[j];
+            }
+            int sum= first+second;
+            if(sum==target){
+                system.out.println(nums[i,j]);
+            }
+        }
+        
+    }
+}
