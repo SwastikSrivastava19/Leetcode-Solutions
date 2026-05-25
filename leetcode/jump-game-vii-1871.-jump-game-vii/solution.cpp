be28fa79bad473aad4171dd4db351f455c4b7eb8
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-25 22:12:18 +0530

    LeetCode: 1871. Jump Game VII (jump-game-vii)

---
+++ leetcode/jump-game-vii-1871.-jump-game-vii/solution.cpp
@@ -0,0 +1,11 @@
+// Problem: 1871. Jump Game VII
+// URL: https://leetcode.com/problems/jump-game-vii/
+// Language: cpp
+// Submission ID: dom-1779727333811
+// Submitted At: 2026-05-25T16:42:14.049Z
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        
+    }
+};
