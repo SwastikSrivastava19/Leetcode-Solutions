23353342c4f56221274386e46d875d91faaaee63
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-27 11:32:30 +0530

    LeetCode: 3120. Count the Number of Special Characters I (count-the-number-of-special-characters-i)

---
+++ leetcode/count-the-number-of-special-characters-i-3120.-count-the-number-of-special-characters-i/solution.cpp
@@ -0,0 +1,19 @@
+// Problem: 3120. Count the Number of Special Characters I
+// URL: https://leetcode.com/problems/count-the-number-of-special-characters-i/
+// Language: cpp
+// Submission ID: dom-1779861745759
+// Submitted At: 2026-05-27T06:02:25.837Z
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<char , int> freq;
+        for(auto x : word) freq[x]++;
+
+        a - 2
+        A - 1
+        b - 1
+        B - 1
+        c - 1
+        C - 1
+    }
+};
