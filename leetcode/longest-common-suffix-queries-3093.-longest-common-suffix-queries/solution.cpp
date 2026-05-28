46923e91ec4e2d92b8bd5b46341c1ce8193f024a
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-28 22:03:38 +0530

    LeetCode: 3093. Longest Common Suffix Queries (longest-common-suffix-queries)

---
+++ leetcode/longest-common-suffix-queries-3093.-longest-common-suffix-queries/solution.cpp
@@ -0,0 +1,11 @@
+// Problem: 3093. Longest Common Suffix Queries
+// URL: https://leetcode.com/problems/longest-common-suffix-queries/
+// Language: cpp
+// Submission ID: dom-1779985941504
+// Submitted At: 2026-05-28T16:32:21.577Z
+class Solution {
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
+        
+    }
+};
