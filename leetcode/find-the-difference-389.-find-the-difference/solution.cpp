b38ea79f730f433a13487f218c740b4297d5c2ec
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-05-31 15:24:34 +0530

    LeetCode: 389. Find the Difference (find-the-difference)

--- leetcode/find-the-difference-389.-find-the-difference/solution.cpp
+++ leetcode/find-the-difference-389.-find-the-difference/solution.cpp
@@ -1,11 +1,20 @@
 // Problem: 389. Find the Difference
 // URL: https://leetcode.com/problems/find-the-difference/
 // Language: cpp
-// Submission ID: dom-1780220953069
-// Submitted At: 2026-05-31T09:49:13.230Z
+// Submission ID: dom-1780221270109
+// Submitted At: 2026-05-31T09:54:30.174Z
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        
+        int asum = 0;
+        int bsum = 0;
+        for(auto ch : s){
+            asum += ch;
+        }
+        for(auto it : t) bsum += it;
+
+        int diff = abs(asum - bsum);
+
+        return diff;
     }
 };
