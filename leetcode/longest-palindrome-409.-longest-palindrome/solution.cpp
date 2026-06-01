8aeee67e5cbfeabcdb45181298d02a2ebb37d52c
Swastik Srivastava <168284278+SwastikSrivastava19@users.noreply.github.com>
2026-06-01 16:29:16 +0530

    LeetCode: 409. Longest Palindrome (longest-palindrome)

--- leetcode/longest-palindrome-409.-longest-palindrome/solution.cpp
+++ leetcode/longest-palindrome-409.-longest-palindrome/solution.cpp
@@ -1,11 +1,27 @@
 // Problem: 409. Longest Palindrome
 // URL: https://leetcode.com/problems/longest-palindrome/
 // Language: cpp
-// Submission ID: dom-1780311284932
-// Submitted At: 2026-06-01T10:54:44.998Z
+// Submission ID: dom-1780311551369
+// Submitted At: 2026-06-01T10:59:11.503Z
 class Solution {
 public:
     int longestPalindrome(string s) {
-        
+        // a -> 1
+        // b -> 1
+        // c -> 4
+        // d -> 2
+
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+        for(char ch : s) {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+                oddCount++;
+            else    
+                oddCount--;
+        }
+        if(oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
     }
 };
